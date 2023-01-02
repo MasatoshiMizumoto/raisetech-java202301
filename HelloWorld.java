5289e333388cb40d78a5502a07a7ed49704048f2
MasatoshiMizumoto <51846213+MasatoshiMizumoto@users.noreply.github.com>
2023-01-02 21:29:05 +0900

    add HelloWorld file

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld { // クラス宣言。クラス名はファイル名と同じにする
+    public static void main(String[] args) { // メインメソッド。プログラムの開始地点。引数はString型の配列
+      System.out.println("#####1回#####"); // stdoutに文字列を出力
+      System.out.println("Hello, World");
+      System.out.println("#####20回#####"); // stdoutに文字列を出力
+      for (int i = 0; i < 20; i++) {
+        if (i <= 10) {
+          System.out.println("Hello, World");
+        }else {
+          System.out.println("Hello");
+        }
+      }
+    }
+}
