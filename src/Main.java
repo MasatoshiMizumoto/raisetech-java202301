1738e9c4008d6be46294e02c40ea8b631d10603d
MasatoshiMizumoto <51846213+MasatoshiMizumoto@users.noreply.github.com>
2023-01-03 20:17:17 +0900

    use printf and enemyname fix

--- src/Main.java
+++ src/Main.java
@@ -6,25 +6,28 @@ public class Main {
 
   public static void main(String[] args) {
     Monster enemy = new Monster("スライム", new Random().nextInt(10 - 1) + 1);
-    System.out.println(
-        "+--------------------+\n" + enemy.getName() + "があらわれた！" + "\n体力:" + enemy.getHealth()
-            + "\n+--------------------+\n");
+    System.out.printf("""
+        +--------------------+
+        %s　が　あらわれた！
+        体力: %d
+        +--------------------+
+        """, enemy.getName(), enemy.getHealth());
 
     // enemyの体力が0になるまで攻撃を繰り返す
     for (int i = 0; enemy.getHealth() > 0; i++) {
       int damage = new Random().nextInt(5);
       System.out.println((i + 1) + "回目のこうげき！");
       if (damage == 0) {
-        System.out.println("ミス！敵にダメージを与えられない！" + "\n");
+        System.out.println("ミス！　" + enemy.getName() + "　にダメージを与えられない！" + "\n");
       } else {
         System.out.println(damage + "のダメージ！");
         enemy.makeDamage(damage);
       }
       if (enemy.getHealth() <= 0) {
-        System.out.println("\nスライムを倒した！");
+        System.out.println("\n" + enemy.getName() + "　を倒した！");
       } else {
-        System.out.println("\nまだ敵は健在だ！");
-        System.out.println("スライムの体力：" + enemy.getHealth() + "\n");
+        System.out.println("\nまだ　" + enemy.getName() + "　は健在だ！");
+        System.out.println(enemy.getName() + "　の体力：" + enemy.getHealth() + "\n");
       }
     }
   }
